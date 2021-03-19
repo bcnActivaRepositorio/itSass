975193eeebf5883848a24a55547d441e9795db6e
naranjito72 <theclick36@gmail.com>
2021-03-19 13:56:26 +0100

    fixing errors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 window.addEventListener("load", function(){
     console.log("Web cargada");
-})
-// onscroll
-window.onscroll = ( function() {
-  sticky(); 
 });
-
-function sticky(){
-    ($(window).scrollTop() >= 0) ? $('header').addClass('mySticky') : $('header').removeClass('mySticky');
-}
